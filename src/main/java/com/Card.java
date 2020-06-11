c31d9dc7d6f541d9e7ba09a99fa8d05de6b8e5a4
kooooichi24 <ex.kouichi1224@gmail.com>
2020-06-12 00:20:24 +0900

    カードクラスをenumに変更して、52pairのカードの定数を設定しました。

--- src/main/java/com/Card.java
+++ src/main/java/com/Card.java
@@ -1,14 +1,80 @@
 package main.java.com;
 
-public class Card {
+public enum Card {
+    SPADE_A("♠", "A", 1),
+    SPADE_2("♠", "2", 2),
+    SPADE_3("♠", "3", 3),
+    SPADE_4("♠", "4", 4),
+    SPADE_5("♠", "5", 5),
+    SPADE_6("♠", "6", 6),
+    SPADE_7("♠", "7", 7),
+    SPADE_8("♠", "8", 8),
+    SPADE_9("♠", "9", 9),
+    SPADE_10("♠", "10", 10),
+    SPADE_J("♠", "J", 10),
+    SPADE_Q("♠", "Q", 10),
+    SPADE_K("♠", "K", 10),
+    HEART_A("❤︎", "A", 1),
+    HEART_2("❤︎︎", "2", 2),
+    HEART_3("❤︎︎", "3", 3),
+    HEART_4("❤︎", "4", 4),
+    HEART_5("❤︎", "5", 5),
+    HEART_6("❤︎︎", "6", 6),
+    HEART_7("❤︎", "7", 7),
+    HEART_8("❤︎", "8", 8),
+    HEART_9("❤︎", "9", 9),
+    HEART_10("❤︎", "10", 10),
+    HEART_J("❤︎", "J", 10),
+    HEART_Q("❤︎", "Q", 10),
+    HEART_K("❤︎", "K", 10),
+    DIAMOND_A("♦", "A", 1),
+    DIAMOND_2("♦︎", "2", 2),
+    DIAMOND_3("♦︎", "3", 3),
+    DIAMOND_4("♦︎", "4", 4),
+    DIAMOND_5("♦︎", "5", 5),
+    DIAMOND_6("♦︎", "6", 6),
+    DIAMOND_7("♦︎", "7", 7),
+    DIAMOND_8("♦︎", "8", 8),
+    DIAMOND_9("♦︎", "9", 9),
+    DIAMOND_1("♦︎", "10", 10),
+    DIAMOND_J("♦︎", "J", 10),
+    DIAMOND_Q("♦︎", "Q", 10),
+    DIAMOND_K("♦︎", "K", 10),
+    CLUB_A("♣︎", "A", 1),
+    CLUB_2("♣︎︎", "2", 2),
+    CLUB_3("♣︎", "3", 3),
+    CLUB_4("♣︎︎", "4", 4),
+    CLUB_5("♣︎", "5", 5),
+    CLUB_6("♣︎", "6", 6),
+    CLUB_7("♣︎", "7", 7),
+    CLUB_8("♣︎", "8", 8),
+    CLUB_9("♣︎", "9", 9),
+    CLUB_1("♣︎", "10", 10),
+    CLUB_J("♣︎", "J", 10),
+    CLUB_Q("♣︎", "Q", 10),
+    CLUB_K("♣︎", "K", 10);
+
+
     private String suit; // トランプの記号はスートと呼ばれる
     private String rank;
     private int value;
 
-    public Card(String suit, String rank, int value) {
+    private Card(String suit, String rank, int value) {
         this.suit = suit;
         this.rank = rank;
         this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     @Override
--- src/main/java/com/Deck.java
+++ src/main/java/com/Deck.java
@@ -3,6 +3,8 @@ package main.java.com;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.shuffle;
+
 public class Deck {
     private List<Card> deck;
 
@@ -12,62 +14,12 @@ public class Deck {
     }
 
     // deckに52枚のカードをセットするメソッド
+    // shuffleも追加でするよん
     public void setUp() {
-        // 最初の謎
-        // {"♠", "A", 1} の形式で52ペア作成して、Deckクラスに代入したい。。。
-        // ひとまずやばいやつで
-        deck.add(new Card("♠", "A", 1));
-        deck.add(new Card("♠", "2", 2));
-        deck.add(new Card("♠", "3", 3));
-        deck.add(new Card("♠", "4", 4));
-        deck.add(new Card("♠", "5", 5));
-        deck.add(new Card("♠", "6", 6));
-        deck.add(new Card("♠", "7", 7));
-        deck.add(new Card("♠", "8", 8));
-        deck.add(new Card("♠", "9", 9));
-        deck.add(new Card("♠", "10", 10));
-        deck.add(new Card("♠", "J", 10));
-        deck.add(new Card("♠", "Q", 10));
-        deck.add(new Card("♠", "K", 10));
-        deck.add(new Card("❤︎", "A", 1));
-        deck.add(new Card("❤︎︎", "2", 2));
-        deck.add(new Card("❤︎︎", "3", 3));
-        deck.add(new Card("❤︎", "4", 4));
-        deck.add(new Card("❤︎", "5", 5));
-        deck.add(new Card("❤︎︎", "6", 6));
-        deck.add(new Card("❤︎", "7", 7));
-        deck.add(new Card("❤︎", "8", 8));
-        deck.add(new Card("❤︎", "9", 9));
-        deck.add(new Card("❤︎", "10", 10));
-        deck.add(new Card("❤︎", "J", 10));
-        deck.add(new Card("❤︎", "Q", 10));
-        deck.add(new Card("❤︎", "K", 10));
-        deck.add(new Card("♦", "A", 1));
-        deck.add(new Card("♦︎", "2", 2));
-        deck.add(new Card("♦︎", "3", 3));
-        deck.add(new Card("♦︎", "4", 4));
-        deck.add(new Card("♦︎", "5", 5));
-        deck.add(new Card("♦︎", "6", 6));
-        deck.add(new Card("♦︎", "7", 7));
-        deck.add(new Card("♦︎", "8", 8));
-        deck.add(new Card("♦︎", "9", 9));
-        deck.add(new Card("♦︎", "10", 10));
-        deck.add(new Card("♦︎", "J", 10));
-        deck.add(new Card("♦︎", "Q", 10));
-        deck.add(new Card("♦︎", "K", 10));
-        deck.add(new Card("♣︎", "A", 1));
-        deck.add(new Card("♣︎︎", "2", 2));
-        deck.add(new Card("♣︎", "3", 3));
-        deck.add(new Card("♣︎︎", "4", 4));
-        deck.add(new Card("♣︎", "5", 5));
-        deck.add(new Card("♣︎", "6", 6));
-        deck.add(new Card("♣︎", "7", 7));
-        deck.add(new Card("♣︎", "8", 8));
-        deck.add(new Card("♣︎", "9", 9));
-        deck.add(new Card("♣︎", "10", 10));
-        deck.add(new Card("♣︎", "J", 10));
-        deck.add(new Card("♣︎", "Q", 10));
-        deck.add(new Card("♣︎", "K", 10));
+        for (Card card : Card.values()) {
+            deck.add(card);
+        }
+        shuffle(deck);
     }
 
     public List<Card> getDeck() {
